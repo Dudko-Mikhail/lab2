5daf6857e5dee1c067d640045eaed6184fd4af70
Dudko Mikhail <dudkomikhail01@gmail.com>
2021-10-22 23:53:16 +0300

    key changes

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.lab2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView orientationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        orientationText = this.findViewById(R.id.orientationText);
+        updateOrientationText(getResources().getConfiguration().orientation);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        updateOrientationText(newConfig.orientation);
+    }
+
+    private void updateOrientationText(int orientation) {
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            orientationText.setText(R.string.landscape_view);
+        }
+        else if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            orientationText.setText(R.string.portrait_view);
+        }
+        else {
+            orientationText.setText(R.string.unknown_orientation);
+        }
     }
 }
